3bbb00a2b95483ca03119c14cc74d87b296cdbe0
muhamadrivaldy21vr20-coder <muhamadrivaldy21vr20@gmail.com>
2026-01-31 18:32:02 +0700

    Initialize App component with routing setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/telier-secret-cms" element={<Admin />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+export default App;
